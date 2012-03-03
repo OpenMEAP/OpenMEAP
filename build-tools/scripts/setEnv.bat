eb31b2f023c423d36e63189a47665dbc16798f9b
Jon Schang <jschang@openmeap.com>
2012-03-03 13:15:17 -0500

    Working on making the JPA properties more externally configurable.

--- build-tools/scripts/setEnv.bat
+++ build-tools/scripts/setEnv.bat
@@ -7,5 +7,19 @@ rem  OpenMEAP
 set ANDROID_SDK_HOME=C:\Users\schang\apps\android-sdk-windows
 set OPENMEAP_HOME=C:\Users\schang\openmeap.dev\openmeap
 
+rem Required on services nodes.  Is used as an identity for a cluster node
+rem set OPENMEAP_CLUSTER_NODE_URL_PREFIX=http://localhost:8080/openmeap-services-web
+
+rem Database settings.  Can also be passed as system properties.
+rem set OPENMEAP_JAVA_ENV=javase
+rem set OPENMEAP_JPA_SHOWSQL=false
+rem set OPENMEAP_JPA_GENERATEDDL=update
+rem set OPENMEAP_JPA_DIALECT=org.hibernate.dialect.SQLite3Dialect
+rem set OPENMEAP_JDBC_DRIVERCLASS=org.sqlite.JDBC
+rem set OPENMEAP_JDBC_URL=jdbc:mysql://192.168.1.2:3306/openmeap
+rem set OPENMEAP_JDBC_URL=jdbc:sqlite:/tmp/openmeap.db
+rem set OPENMEAP_JDBC_USERNAME=openmeap
+rem set OPENMEAP_JDBC_PASSWORD=openmeap
+
 rem  Path extending
 set PATH=%PATH%;%ANT_HOME%\bin;%OPENMEAP_HOME%\build-tools\scripts
--- build-tools/scripts/setEnv.sh
+++ build-tools/scripts/setEnv.sh
@@ -5,7 +5,21 @@ declare -x ANT_HOME=/usr/share/ant
 
 # OpenMEAP
 declare -x ANDROID_SDK_HOME=/Users/schang/apps/android-sdk-mac_x86
-declare -x OPENMEAP_HOME=/Users/schang/openmeap
+declare -x OPENMEAP_HOME=/Users/schang/openmeap.dev/openmeap
+
+# Required on services nodes.  Is used as an identity for a cluster node
+#declare -x OPENMEAP_CLUSTER_NODE_URL_PREFIX=http://localhost:8080/openmeap-services-web
+
+# Database settings.  Can also be passed as system properties.
+#declare -x OPENMEAP_JAVA_ENV=javase
+#declare -x OPENMEAP_JPA_SHOWSQL=false
+#declare -x OPENMEAP_JPA_GENERATEDDL=update
+#declare -x OPENMEAP_JPA_DIALECT=org.hibernate.dialect.SQLite3Dialect
+#declare -x OPENMEAP_JDBC_DRIVERCLASS=org.sqlite.JDBC
+#declare -x OPENMEAP_JDBC_URL=jdbc:mysql://192.168.1.2:3306/openmeap
+#declare -x OPENMEAP_JDBC_URL=jdbc:sqlite:/tmp/openmeap.db
+#declare -x OPENMEAP_JDBC_USERNAME=openmeap
+#declare -x OPENMEAP_JDBC_PASSWORD=openmeap
 
 # Path extending
 declare -x PATH=$PATH:$ANT_HOME/bin:$OPENMEAP_HOME/build-tools/scripts
