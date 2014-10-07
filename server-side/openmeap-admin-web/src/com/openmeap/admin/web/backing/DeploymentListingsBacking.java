39404b3a84e6c2337755a459e7bcf3d67e6e727b
Rob Thacher <rob@openmeap.com>
2014-10-07 14:05:29 -0400

    Updates for Release 1.5.0

--- server-side/openmeap-admin-web/src/com/openmeap/admin/web/backing/DeploymentListingsBacking.java
+++ server-side/openmeap-admin-web/src/com/openmeap/admin/web/backing/DeploymentListingsBacking.java
@@ -108,7 +108,7 @@ public class DeploymentListingsBacking extends AbstractTemplatedSectionBacking {
 					modelManager.begin();
 					depl = modelManager.addModify(depl,events);
 					modelManager.commit(events);
-					events.add( new MessagesEvent("Deployment successfully completed!”) );
+					events.add( new MessagesEvent("Deployment successfully completed.") );
 				} catch (Exception pe) {
 					modelManager.rollback();
 					Throwable root = ExceptionUtils.getRootCause(pe);
